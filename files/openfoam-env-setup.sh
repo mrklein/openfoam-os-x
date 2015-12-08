318feee347b082690b9f66a265c2c3bec70329fb
Alexey Matveichev <alexey.matveichev@ymail.com>
2015-12-08 22:49:51 +0100

    Update
    Cosmetic fixes in environment setup functions.

--- files/openfoam-env-setup.sh
+++ files/openfoam-env-setup.sh
@@ -7,21 +7,21 @@
 # $ cat '2.X.Y' > .OpenFOAM/OpenFOAM-release
 # If you'd like to switch environment between versions use of2xy commands.
 
-readonly FOAM_MOUNT_POINT=${FOAM_MOUNT_POINT:-"$HOME/OpenFOAM"}
-readonly FOAM_RELEASE_FILE=${FOAM_RELESE_FILE:-"$HOME/.OpenFOAM/OpenFOAM-release"}
-readonly FOAM_DISK_IMAGE=${FOAM_DISK_IMAGE:-"$HOME/OpenFOAM.sparsebundle"}
+readonly FOAM_MOUNT_POINT="${FOAM_MOUNT_POINT:-"$HOME/OpenFOAM"}"
+readonly FOAM_RELEASE_FILE="${FOAM_RELESE_FILE:-"$HOME/.OpenFOAM/OpenFOAM-release"}"
+readonly FOAM_DISK_IMAGE="${FOAM_DISK_IMAGE:-"$HOME/OpenFOAM.sparsebundle"}"
 
 mount_disk_image () {
-	local oldpwd=$(pwd)
-	cd $HOME
+	local oldpwd="$(pwd)"
+	cd "$HOME"
 	# Attempt to mount image
 	hdiutil attach -quiet -mountpoint "$FOAM_MOUNT_POINT" "$FOAM_DISK_IMAGE"
-	cd $oldpwd
+	cd "$oldpwd"
 	return 0
 }
 
 main () {
-	[ -f $FOAM_RELEASE_FILE ] || return 1
+	[ -f "$FOAM_RELEASE_FILE" ] || return 1
 
 	local release="$(cat "$FOAM_RELEASE_FILE")"
 	local bashrc="$FOAM_MOUNT_POINT/OpenFOAM-$release/etc/bashrc"
@@ -30,7 +30,7 @@ main () {
 
 	if [ -f "$bashrc" ]
     then
-		source $bashrc WM_NCOMPPROCS="$(sysctl -n hw.ncpu)"
+		source "$bashrc" WM_NCOMPPROCS="$(sysctl -n hw.ncpu)"
 	else
 		echo "OpenFOAM $release doesn't seem to be installed."
 	fi
@@ -42,11 +42,11 @@ ofxxx () {
 	[ -n "$WM_PROJECT_DIR" ] && . "$WM_PROJECT_DIR/etc/config/unset.sh"
 	local bashrc="$FOAM_MOUNT_POINT/OpenFOAM-$release/etc/bashrc"
 	if [ -f "$bashrc" ]; then
-		source $bashrc WM_NCOMPPROCS=$(sysctl -n hw.ncpu)
+		source "$bashrc" WM_NCOMPPROCS="$(sysctl -n hw.ncpu)"
 	else
 		mount_disk_image
 		if [ -f "$bashrc" ]; then
-			source $bashrc WM_NCOMPPROCS=$(sysctl -n hw.ncpu)
+			source "$bashrc" WM_NCOMPPROCS="$(sysctl -n hw.ncpu)"
 		else
 			echo "OpenFOAM $release doesn't seem to be installed."
 		fi
@@ -54,55 +54,55 @@ ofxxx () {
 }
 
 of22x () {
-	ofxxx '2.2.x'
+	ofxxx "2.2.x"
 }
 
 export -f of22x
 
 of231 () {
-	ofxxx '2.3.1'
+	ofxxx "2.3.1"
 }
 
 export -f of231
 
 of23x () {
-	ofxxx '2.3.x'
+	ofxxx "2.3.x"
 }
 
 export -f of23x
 
 of240 () {
-	ofxxx '2.4.0'
+	ofxxx "2.4.0"
 }
 
 export -f of240
 
 of24x () {
-	ofxxx '2.4.x'
+	ofxxx "2.4.x"
 }
 
 export -f of24x
 
-of22x () {
-	ofxxx '2.2.x'
-}
-
-export -f of22x
-
 of300() {
-	ofxxx '3.0.0'
+	ofxxx "3.0.0"
 }
 
 export -f of300
 
 of30x() {
-	ofxxx '3.0.x'
+	ofxxx "3.0.x"
 }
 
 export -f of30x
 
+ofdev() {
+    ofxxx "dev"
+}
+
+export -f ofdev
+
 pf () {
-	paraFoam > /dev/null 2>&1 &
+	paraFoam -builtin > /dev/null 2>&1 &
 }
 
 main
