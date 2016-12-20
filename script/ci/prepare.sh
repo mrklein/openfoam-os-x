d3d80d8344798e4e23f1a8276a0fd9a3279bc082
Alexey Matveichev <github@matveichev.com>
2016-12-20 21:15:13 +0100

    Travis-CI: change folder to $TRAVIS_BUILD_DIR

--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -12,19 +12,6 @@ create_prefs()
     echo 'export WM_NCOMPPROCS=$(sysctl -n hw.ncpu)' >> "$HOME/.OpenFOAM/prefs.sh"
 }
 
-prepare_release()
-{
-    cd $HOME
-    curl -L http://downloads.sourceforge.net/foam/OpenFOAM-$VERSION.tgz > OpenFOAM-$VERSION.tgz
-    cd "$HOME/OpenFOAM"
-    tar xzf "$HOME/OpenFOAM-$VERSION.tgz"
-    cd "$HOME/OpenFOAM/OpenFOAM-$VERSION"
-    cp $TRAVIS_BUILD_DIR/OpenFOAM-$VERSION.patch OpenFOAM.patch
-    git apply OpenFOAM.patch
-    echo "\n\nc++WARN += -Wno-unused-comparison -Wno-unknown-warning-option" >> wmake/rules/darwin64Clang/c++
-    return 0
-}
-
 prepare_git_version()
 {
     cd "$HOME/OpenFOAM"
@@ -35,16 +22,10 @@ prepare_git_version()
     git checkout -b local-install $initial_commit
     cp $TRAVIS_BUILD_DIR/$patch_file OpenFOAM.patch
     git apply OpenFOAM.patch
-    echo "\n\nc++WARN += -Wno-unused-comparison -Wno-unknown-warning-option" >> wmake/rules/darwin64Clang/c++
     return 0
 }
 
-if [ "${VERSION: -1}" = "x" -o "$VERSION" = "dev" ]
-then
-    prepare_git_version $VERSION
-else
-    prepare_release $VERSION
-fi
+prepare_git_version $VERSION
 
 create_prefs
 
